7930a590d68c9985156c2b4eb6806f565dcb860d
“Karera-o” <olivierkarera2020@gmail.com>
2022-08-26 10:24:31 +0200

    adding signinForm

--- src/ui/components/navbar/navbar.jsx
+++ src/ui/components/navbar/navbar.jsx
@@ -11,14 +11,19 @@ function Navbar() {
             <img src={logo} alt="" />
         </div>
         <div id="navUrl">
-            <ul>
-            
+            <ul id="list">
                 <li><a href="/">Company</a></li>
                 <li><a href="/">Investors</a></li>
                 <li><a href="/">Career</a></li>
                 <li><a href="/">Register</a></li>
                 <li><a href="/">Donate</a></li>
                 <li><a href="/">Login</a></li>
+                {/* <li className="li" ><a href="/">Company</a></li>
+                <li className="li"><a href="/">Investors</a></li>
+                <li className="li"><a href="/">Career</a></li>
+                <li className="li"><a href="/">Register</a></li>
+                <li className="li"><a href="/"></a></li>
+                <li className="li"><a href="/">Login</a></li> */}
                 {/* <h1>hello world navbar</h1> */}
             </ul>
         </div>
