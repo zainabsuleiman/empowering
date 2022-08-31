33fd08fb9aa3f759b17079f617c7856655f47fa8
Karera-o <olivierkarera2020@gmail.com>
2022-08-31 07:35:29 +0200

    Finishing leftside on dashboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-
-import React from 'react';
 import './App.css';
-import SigninPage from './ui/pages/signinPage/signinPage';
+import Header from './ui/components/dashboard/header/header';
+import LeftSide from './ui/components/dashboard/leftSide/leftSide';
+import Dashboard from './ui/pages/investorDashboard/dashboard';
+// import HomePage from './ui/pages/homePage/homePage';
+// import SigninPage from './ui/pages/signinPage/signinPage';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
-      <div>
+      {/* <div> */}
       
-        <SigninPage />
-      </div>
-
+        {/* <SigninPage /> */}
+        {/* <HomePage /> */}
+        
+      {/* </div> */}
+      <Dashboard />
+      {/* <div><LeftSide /></div>  */}
+      
+      {/* <div id='id'><Header /></div>  */}
      
     </div>
   );
